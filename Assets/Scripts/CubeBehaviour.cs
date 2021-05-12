10ec0da4b3e5e6530a3279d4f8ace273c1dd2cfb
JosephPena1 <jgpena2888@gmail.com>
2021-05-12 14:18:48 -0500

    Created EventSystem;

---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// Turns the object on/off
+    /// </summary>
+    public void ToggleCube()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
---
+++ Assets/Scripts/InputBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Event _onSpacePress;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            // The "?" checks if _onSpacePress is null
+            _onSpacePress?.Raise(gameObject);
+    }
+}
